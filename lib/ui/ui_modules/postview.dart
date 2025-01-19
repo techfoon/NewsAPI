f724c77bf0095937fa2ef133a26bea869bb101da
Peeyush <69499613+PeeyushSachan@users.noreply.github.com>
2025-01-19 19:38:36 +0530

    final

--- lib/ui/ui_modules/postview.dart
+++ lib/ui/ui_modules/postview.dart
@@ -1,8 +1,19 @@
+/*
+
+ L = 6 iches
+
+ width = 2.5 *2+1
+
+MediaQuery.of(context).size.width;
+
+MediaQuery.of(context).orientation;
+MediaQuery.of(context).devicePixelRatio;
+
+*/
+
 import 'package:flutter/material.dart';
 
 class Postview extends StatefulWidget {
-  const Postview({super.key});
-
   @override
   State<Postview> createState() => _PostviewState();
 }
@@ -10,225 +21,250 @@ class Postview extends StatefulWidget {
 class _PostviewState extends State<Postview> {
   @override
   Widget build(BuildContext context) {
+    var ScreenHeight = MediaQuery.of(context).size.height;
+    var ScreenWidth = MediaQuery.of(context).size.height;
+
+    var orientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(
-                Icons.arrow_back,
-                size: 30,
-              ),
-              Text(
-                "Breaking News",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              Row(children: [
-                Image.asset(
-                  "assets/icons/share.png",
-                  height: 20,
-                  width: 20,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Icon(
-                  Icons.bookmark_outline_rounded,
-                  size: 20,
-                )
-              ]),
-            ],
+      appBar: AppBar(
+        leading: Icon(
+          Icons.arrow_back,
+          size: 25,
+        ),
+        title: Center(
+          child: Text(
+            "Breaking News",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        actions: [
+          Image.asset(
+            "assets/icons/share.png",
+            width: 20,
           ),
           SizedBox(
-            height: 20,
+            width: 10,
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 241, 243, 245),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Center(
+          Icon(
+            Icons.bookmark_outline_rounded,
+            size: 20,
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(2),
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(238, 243, 255, 255),
+                              borderRadius: BorderRadius.circular(12)),
                           child: Image.asset(
                             "assets/icons/cnbcNews.png",
-                            height: 30,
-                            width: 30,
+                            height: 24,
                           ),
                         ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "Cnbc News",
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: const Color.fromARGB(97, 66, 58, 58)),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Image.asset(
+                          "assets/icons/rec.png",
+                          height: 10,
+                          width: 10,
+                          color: const Color.fromARGB(97, 66, 58, 58),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "1h Ago",
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: const Color.fromARGB(97, 66, 58, 58)),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(7),
+                        color: const Color.fromARGB(238, 243, 255, 255),
                       ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        "Cnbc News",
+                      width: 70,
+                      height: 30,
+                      child: Center(
+                          child: Text(
+                        "Follow",
                         style: TextStyle(
-                            color: const Color.fromARGB(97, 66, 58, 58)),
-                      ),
-                      SizedBox(
-                        width: 5,
+                            fontSize: 13,
+                            color: const Color.fromARGB(255, 31, 30, 30)),
+                      )),
+                    )
+                  ],
+                ),
+                height: Orientation.portrait == orientation
+                    ? ScreenHeight * 0.07
+                    : ScreenHeight * 0.15,
+              ),
+              Container(
+                child: Expanded(
+                  child: Text(
+                    "Scientist Just Found The Lost Species of Jellyfish The went Extinct 25 Million Years Ago!",
+                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                height: Orientation.portrait == orientation
+                    ? ScreenHeight * 0.15
+                    : ScreenHeight * 0.23,
+                width: Orientation.portrait == orientation
+                    ? ScreenWidth
+                    : ScreenWidth * 2,
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    Text(
+                      "Monday,",
+                      style: TextStyle(
+                          fontSize: 13,
+                          color: const Color.fromARGB(97, 66, 58, 58)),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "24 March 2024",
+                      style: TextStyle(
+                          fontSize: 13,
+                          color: const Color.fromARGB(97, 66, 58, 58)),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Image.asset(
+                      "assets/icons/rec.png",
+                      height: 13,
+                      color: const Color.fromARGB(97, 66, 58, 58),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Image.asset(
+                      "assets/icons/heart.png",
+                      height: 13,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "5.2K",
+                      style: TextStyle(
+                          fontSize: 13,
+                          color: const Color.fromARGB(97, 66, 58, 58)),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Icon(
+                      Icons.chat_outlined,
+                      size: 13,
+                      color: const Color.fromARGB(97, 66, 58, 58),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "23K",
+                      style: TextStyle(
+                          color: const Color.fromARGB(97, 66, 58, 58)),
+                    )
+                  ],
+                ),
+                height: Orientation.portrait == orientation
+                    ? ScreenHeight * 0.07
+                    : ScreenHeight * 0.15,
+              ),
+              Container(
+                height: Orientation.portrait == orientation
+                    ? ScreenHeight * 0.30
+                    : ScreenHeight * 0.62,
+                width: double.infinity,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    "assets/img/thumbnail.png",
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(7),
+                              color: const Color.fromARGB(238, 243, 255, 255),
+                            ),
+                            width: 70,
+                            height: 30,
+                            child: Center(
+                                child: Text(
+                              "Animal",
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: const Color.fromARGB(255, 31, 30, 30)),
+                            )),
+                          ),
+                          SizedBox(width: 15),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(7),
+                              color: const Color.fromARGB(238, 243, 255, 255),
+                            ),
+                            width: 70,
+                            height: 30,
+                            child: Center(
+                                child: Text(
+                              "Nature",
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: const Color.fromARGB(255, 31, 30, 30)),
+                            )),
+                          )
+                        ],
                       ),
                       Image.asset(
-                        "assets/icons/rec.png",
-                        height: 10,
-                        width: 10,
-                        color: const Color.fromARGB(97, 66, 58, 58),
+                        "assets/icons/heart.png",
+                        width: 16,
                       ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        "1h Ago",
-                        style: TextStyle(
-                            color: const Color.fromARGB(97, 66, 58, 58)),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(7),
-                      color: const Color.fromARGB(255, 241, 243, 245),
-                    ),
-                    width: 70,
-                    height: 40,
-                    child: Center(
-                        child: Text(
-                      "Follow",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(97, 66, 58, 58)),
-                    )),
-                  ),
-                ]),
+                    ]),
+              ),
+            ],
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Scientist Just Found The Lost\nSpecies of Jellyfish The went\nExtinct 25 Million Years Ago!",
-            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Row(
-              children: [
-                Text(
-                  "Monday,",
-                  style: TextStyle(color: const Color.fromARGB(97, 66, 58, 58)),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  "24 March 2024",
-                  style: TextStyle(color: const Color.fromARGB(97, 66, 58, 58)),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Image.asset(
-                  "assets/icons/rec.png",
-                  height: 20,
-                  width: 20,
-                  color: const Color.fromARGB(97, 66, 58, 58),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Image.asset(
-                  "assets/icons/heart.png",
-                  height: 20,
-                  width: 20,
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  "5.2K",
-                  style: TextStyle(color: const Color.fromARGB(97, 66, 58, 58)),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Icon(
-                  Icons.chat_outlined,
-                  color: const Color.fromARGB(97, 66, 58, 58),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  "23K",
-                  style: TextStyle(color: const Color.fromARGB(97, 66, 58, 58)),
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-              height: 200,
-              width: double.infinity,
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(20)),
-              child: Image.asset(
-                "assets/icons/images.jpeg",
-                fit: BoxFit.fill,
-              )),
-          SizedBox(
-            height: 20,
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Row(
-              children: [
-                Container(
-                  width: 70,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 241, 243, 245),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Center(
-                      child: Text(
-                    "Animal",
-                    style:
-                        TextStyle(color: const Color.fromARGB(97, 66, 58, 58)),
-                  )),
-                ),
-                SizedBox(
-                  width: 30,
-                ),
-                Container(
-                    width: 70,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color.fromARGB(255, 241, 243, 245),
-                    ),
-                    child: Center(
-                        child: Text(
-                      "Nature",
-                      style: TextStyle(
-                          color: const Color.fromARGB(97, 66, 58, 58)),
-                    )))
-              ],
-            ),
-            Image.asset(
-              "assets/icons/heart.png",
-              height: 20,
-              width: 20,
-            ),
-          ]),
-        ]),
+        ),
       ),
     );
   }
